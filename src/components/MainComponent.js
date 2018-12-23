710a28190de06042583bf2d25a2f8356e73fec27
Mark Nelson <mark.x.nelson@oracle.com>
2018-12-23 18:44:30 -0500

    single page applications part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,21 +3,31 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            dishes: DISHES
+            dishes: DISHES,
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS
         }
     }
 
     render() {
         const HomePage = () => {
-            return(
-                <Home />
+            return (
+                <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                    promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]} />
             )
         }
 
@@ -27,6 +37,7 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
